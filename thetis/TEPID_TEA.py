fca8b6d7cabd68ca4fc1e8c2921be4b20786f1e7
xyr115 <42252570+xyr115@users.noreply.github.com>
2018-10-09 16:43:37 -0400

    Add provisioner file for grouping the appropriate exporter narrtives per node

---
+++ thetis/TEPID_TEA.py
@@ -0,0 +1,17 @@
+#/usr/bin/python
+
+#Targeted Export Provisioner
+targets = [
+    'elasticsearch',
+    'beanstalk',
+    'consul',
+    'mesos',
+    'mongo',
+    'cassandra',
+    'mysql',
+    'nginx',
+    'postgres',
+    'redis',
+    'rabbitmq',
+    'varnish'
+]
